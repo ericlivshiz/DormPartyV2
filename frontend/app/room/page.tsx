2ec8fb4d688335200f6ed9488120ecdb1fa79d0b
Eric Livshiz <ericlivshiz1@gmail.com>
2025-05-30 15:39:13 -0700

    Add some logging

--- frontend/app/room/page.tsx
+++ frontend/app/room/page.tsx
@@ -1,12 +1,11 @@
-'use client'
-import * as React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-
+"use client";
+import * as React from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 function randomID(len: number) {
-  let result = '';
+  let result = "";
   if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+  var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
     maxPos = chars.length,
     i;
   len = len || 5;
@@ -16,50 +15,63 @@ function randomID(len: number) {
   return result;
 }
 
-export function getUrlParams(
-  url = window.location.href
-) {
-  let urlStr = url.split('?')[1];
+export function getUrlParams(url?: string) {
+  const urlStr = (url || window.location.href).split("?")[1];
   return new URLSearchParams(urlStr);
 }
 
 export default function App() {
-      const roomID = getUrlParams().get('roomID') || randomID(5);
-      let myMeeting: any = async (element: any) => {
-     // generate Kit Token
-      const appID: number = Number(process.env.APP_ID);
-      const serverSecret: string = String(process.env.SERVER_SECRET);
-      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-
-
-     // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      // start the call
-      zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+  const roomID = getUrlParams().get("roomID") || randomID(5);
+  let myMeeting: any = async (element: any) => {
+    const appID: number = Number(process.env.NEXT_PUBLIC_APP_ID);
+    const serverSecret: string = String(process.env.NEXT_PUBLIC_SERVER_SECRET);
+    console.log(`APP ID: ${appID}`);
+    console.log(`SERVER SECRET: ${serverSecret}`);
+    if (!appID || !serverSecret) {
+      alert("App ID or Server Secret is missing!");
+      return;
+    }
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      randomID(5),
+      randomID(5)
+    );
+    if (!kitToken) {
+      alert("Kit token is invalid!");
+      return;
+    }
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    if (!zp) {
+      alert("ZegoUIKitPrebuilt.create returned undefined!");
+      return;
+    }
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Personal link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
         },
-      });
-
-
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
   };
 
   return (
     <div
       className="myCallContainer"
       ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
+      style={{ width: "100vw", height: "100vh" }}
     ></div>
   );
 }

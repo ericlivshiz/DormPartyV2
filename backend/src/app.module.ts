fd93aebeff568ca18762f2770f7b60d2813bedd3
Eric Livshiz <ericlivshiz1@gmail.com>
2025-05-30 21:21:41 -0700

    connect with railway redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CallModule } from './call/call.module';
 import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Loads `.env` before anything else
+    }),
+
     CallModule,
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
+
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get<string>('REDIS_HOST');
+        const port = configService.get<number>('REDIS_PORT');
+        const password = configService.get<string>('REDIS_PASSWORD');
+
+        console.log('Connecting to Redis with:', {
+          host,
+          port,
+          password: password ? '***' : 'undefined',
+        });
+
+        return {
+          redis: {
+            host,
+            port,
+            password,
+          },
+        };
       },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],

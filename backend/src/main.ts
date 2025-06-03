2be8cd0f91d3aa0c3dcfcfef29b362c6818b8af0
Eric Livshiz <ericlivshiz1@gmail.com>
2025-06-02 20:38:48 -0700

    add origin and logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Allow frontend to connect (especially WebSocket)
-  await app.listen(process.env.PORT || 3005); // Use Railway-assigned port
+  app.enableCors({
+    origin: [
+      'https://dormpartyv2-production.up.railway.app',
+      'https://dormparty-v2-backend-production.up.railway.app',
+    ],
+    credentials: true,
+  });
+  console.log(`Listening on port ${process.env.PORT || 3005}`)
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
